1461283e5aeeacad856202fbf8f36633a45efb96
Ruslan Gureev <ruslanus1989@gmail.com>
2022-09-11 13:07:23 +0300

    Homework done

---
+++ Zadanie_1/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите количество цифр: ");
+int size = int.Parse(Console.ReadLine());
+int count = 0;
+string resul = " ";
+int number1 = 1;
+
+for (int i = 0; i < size; ++i)
+{
+    Console.Write($"Введите {number1} число: ");
+    double number = double.Parse(Console.ReadLine());
+    resul = resul + number + ",";
+    if (number > 0)
+    ++count;
+    ++number1;
+}
+Console.Write($"Среди введенных чисел {resul} больше нуля: {count}");
+
---
+++ Zadanie_2/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Первая прямая задана уравнением y = k1 * x + b1");
+Console.Write("k1 = ");
+double k1 = double.Parse(Console.ReadLine());
+Console.Write("b1 = ");
+double b1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Вторая прямая задана уравнением y = k2 * x + b2");
+Console.Write("k2 = ");
+double k2 = double.Parse(Console.ReadLine());
+Console.Write("b2 = ");
+double b2 = double.Parse(Console.ReadLine());
+
+if (k2 == k1 && b2 == b1)
+{
+    Console.WriteLine("Прямые совпадают");
+}
+else if (k2 == k1 && b2 != b1)
+{
+    Console.WriteLine("Прямые параллельны");
+}
+else
+{
+    double x = (b2 - b1) / (k1 - k2);
+    double y = k1 * x + b1;
+    Console.WriteLine($"Координаты точки пересечения этих прямых ({x},{y})");
+}
+
